2227843a1cbb7c069143c07eb8732c921dd46e30
Sascha Andres <sascha.andres@outlook.com>
2016-12-06 17:42:34 +0100

    Added column range support

--- src/ExcelValueMapper/Configuration/ConfigurationExtentions.cs
+++ src/ExcelValueMapper/Configuration/ConfigurationExtentions.cs
@@ -17,15 +17,43 @@
 // limitations under the License.
 
 namespace ExcelValueMapper.Configuration {
+  using System.Text.RegularExpressions;
   using System.Collections.Generic;
 
   // Helper methods that work on configuration values
   public static class ConfigurationExtentions {
+    private const string RANGE_EXPRESSION = "^(?<from>[0-9]*)-(?<to>[0-9]*)$";
+    private static Regex _Regex = new Regex( RANGE_EXPRESSION );
+
     // Preparing for handling column ranges
     public static IEnumerable<int> GetColumns ( this string columnValue ) {
-      var result = 0;
-      if ( !int.TryParse( columnValue, out result ) ) yield break;
-      yield return result;
+      if ( _Regex.IsMatch( columnValue ) ) {
+        var matchedResult = _Regex.Match( columnValue );
+        var from = 0;
+        var to = 0;
+        if ( !int.TryParse( matchedResult.Groups["from"].Value, out from ) ) {
+          System.Console.WriteLine( $"WARNING: {columnValue} seems to be a range but {matchedResult.Groups["from"].Value} is not parsable" );
+          yield break;
+        }
+        if ( !int.TryParse( matchedResult.Groups["to"].Value, out to ) ) {
+          System.Console.WriteLine( $"WARNING: {columnValue} seems to be a range but {matchedResult.Groups["to"].Value} is not parsable" );
+          yield break;
+        }
+        if (to >= from) {
+          System.Console.WriteLine( $"WARNING: {to} is >= {from}, dropping" );
+          yield break;
+        }
+        for ( int i = from; i <= to; i++ ) {
+          yield return i;
+        }
+      } else {
+        var result = 0;
+        if ( !int.TryParse( columnValue, out result ) ) {
+          System.Console.WriteLine( $"WARNING: {columnValue} is non numeric and not a range" );
+          yield break;
+        }
+        yield return result;
+      }
     }
   }
 }
